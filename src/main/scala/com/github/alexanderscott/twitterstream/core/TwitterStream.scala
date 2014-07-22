4b4974810d64033a30cb0857fe2d432c831f3b84
Alex Ehrnschwender <alexanderscott46@gmail.com>
2014-07-21 22:32:13 -0700

    moves TwitterStreamActor, adds gitignore

--- src/main/scala/com/github/alexanderscott/twitterstream/core/TwitterStream.scala
+++ src/main/scala/com/github/alexanderscott/twitterstream/core/TwitterStream.scala
@@ -68,21 +68,3 @@ trait TweetMarshaller {
   }
 }
 
-object TweetStreamerActor {
-  val twitterUri = Uri("https://stream.twitter.com/1.1/statuses/filter.json")
-}
-
-class TweetStreamerActor(uri: Uri, processor: ActorRef) extends Actor with TweetMarshaller {
-  this: TwitterAuthorization =>
-  val io = IO(Http)(context.system)
-
-  def receive: Receive = {
-    case query: String =>
-      val body = HttpEntity(ContentType(MediaTypes.`application/x-www-form-urlencoded`), s"track=$query")
-      val rq = HttpRequest(HttpMethods.POST, uri = uri, entity = body) ~> authorize
-      sendTo(io).withResponsesReceivedBy(self)(rq)
-    case ChunkedResponseStart(_) =>
-    case MessageChunk(entity, _) => TweetUnmarshaller(entity).fold(_ => (), processor !)
-    case _ =>
-  }
-}
